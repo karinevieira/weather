f64e9d03cac0a185f29f2cd67366730fbe606624
Karine Vieira <kv.vieira1@gmail.com>
2021-11-11 21:03:56 -0300

    add new results

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+if ($('input').val() == ''){
+    $('.result').hide()
+}
+
 $('svg').on('click', function(){
     let city = $('input').val()
+    $('.result').show()
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&appid=5033f01276a4961c65b0685a4186556e&units=metric&lang=pt_br`
 
@@ -12,7 +17,19 @@ $('svg').on('click', function(){
         },
         success: function(json){
             if(json.cod == 200){
-                $('.result').html(`Temperatura: ${json.main.temp} ºC`)
+                $('.result').html(
+                    `
+                    <h3>${json.name}-${json.sys.country}</h3>
+                    <div class="temp">
+                        <p>Temperatura: ${json.main.temp} ºC</p>
+                        <p>Sensação Térmica: ${json.main.feels_like} ºC</p>
+                    </div>
+                    <div class="weather">
+                        <p>${json.weather[0].description}</p>
+                        <img src="http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png">
+                    </div>
+                    `
+                )
             }
         },
         error: function(){
